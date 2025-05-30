3e3613b6ca86eafc5eaf75f3a457922d2122389c
ilincamartole <ilincamartole@gmail.com>
2025-05-30 20:54:18 +0300

    Fisiere noi

--- Utilis.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Block.h"
-
-template <typename T>
-bool poateFiCombinat(const Block<T>& a, const Block<T>& b)
-{
-    return !a.esteGol() && !b.esteGol() && a.getValoare() == b.getValoare();
-}
